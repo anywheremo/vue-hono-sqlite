6ebd9c452590832cd6f0ba03c39f70bfe2746b19
anywheremo <107391143+anywheremo@users.noreply.github.com>
2026-05-25 21:45:40 +0900

    バリデーション追加、ルート見直し（1ファイル化）

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,10 +1,16 @@
 import { sql } from "drizzle-orm"
 import {sqliteTable,text,integer}from "drizzle-orm/sqlite-core"
+import z from "zod";
 export const users=sqliteTable('user',{
   id:integer('id').primaryKey({autoIncrement:true}),
   name:text('name').notNull(),
   email:text('email').notNull().unique(),
   createdAt:text('created_at').default(sql`CURRENT_TIMESTAMP`)
 })
+export const createUserSchema = z.object({
+  name: z.string().min(1, "名前は必須です" ),
+  email: z.email("不正なメールアドレスです。")
+})
+export const updateUserSchema = createUserSchema.partial();
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,54 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { createUser, getUsers } from './handlers/users.js'
-
+import { zValidator } from '@hono/zod-validator'
+import { db } from './db/index.js'
+import { createUserSchema,updateUserSchema,users} from './db/schema.js'
+import { eq } from 'drizzle-orm'
 const app = new Hono().basePath('/api')
 const frontendUrl=process.env.FRONTEND_URL||"http://localhost:5173"
+//cors設定
 app.use('*',cors({
   origin:frontendUrl,
-  allowMethods:["GET","POST","PUT","DELETE","OPTION"],
+  allowMethods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
   credentials:true
 }))
 const routes=app
-.get('/users',getUsers)
-.post('/users',createUser)
+//users
+.get('/users',async(c)=>{
+  try {
+    const allUsers=await db.select().from(users)
+    return c.json(allUsers)
+  } catch (error) {
+    return c.json({success:false,error:"failed to fetch users"},500)
+  }
+})
+.get('/users/:id', async (c) => {
+  try {
+    const id = Number(c.req.param('id'))
+    if (isNaN(id)) {
+      return c.json({ success: false, error: "Invalid ID format" }, 400)
+    }
+    const [user] = await db.select().from(users).where(eq(users.id, id))
+    if (!user) {
+      return c.json({ success: false, error: "User not found" }, 404)
+    }
+    return c.json({ success: true, data: user })
+  } catch (error) {
+    console.error('Fetch user by id error:', error)
+    return c.json({ success: false, error: "failed to fetch user" }, 500)
+  }
+})
+.post('/users',zValidator('json',createUserSchema), async(c)=>{
+  const validate=c.req.valid('json')
+  const [newUser]=await db.insert(users).values({
+      name:validate.name,
+      email:validate.email
+    }).returning()
+  return c.json({success:true,data:newUser},201)
+})
+
+//other
 .get('/', (c) => {
   return c.text('Hello Hono!')
 })
