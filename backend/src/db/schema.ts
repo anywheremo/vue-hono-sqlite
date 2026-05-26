331cbaa5d46802daef116b9426373cedc31219ef
anywheremo <107391143+anywheremo@users.noreply.github.com>
2026-05-26 09:29:57 +0900

    schema.ts を更新

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm"
 import {sqliteTable,text,integer}from "drizzle-orm/sqlite-core"
 import z from "zod";
-export const users=sqliteTable('user',{
+export const users=sqliteTable('users',{
   id:integer('id').primaryKey({autoIncrement:true}),
   name:text('name').notNull(),
   email:text('email').notNull().unique(),
