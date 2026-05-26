72eba56930588fa04fa20e031a8d4af2a77077a8
anywheremo <107391143+anywheremo@users.noreply.github.com>
2026-05-26 09:30:35 +0900

    drizzle.config.ts を更新

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema:"./src/db/schema.ts",
   dialect:"sqlite",
   dbCredentials:{
-    url:process.env.DATABASE_URL||'default.db'
+    url:process.env.DATABASE_URL||'no_env.db'
   }
 })
