a5c8580ed87413f5bf295af772de4b07c7360ef4
anywheremo <107391143+anywheremo@users.noreply.github.com>
2026-05-26 09:29:18 +0900

    index.ts を更新

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -4,5 +4,6 @@ import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as dotenv from 'dotenv'
 import * as schema from './schema.js'
 dotenv.config()
-const sqlite=new Database(process.env.DaTABASE_URL||'local.db')
+const sqlite=new Database(process.env.DaTABASE_URL||'no_env.db')
+sqlite.pragma('journal_mode')
 export const db=drizzle(sqlite,{schema})
